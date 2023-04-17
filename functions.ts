78439e9a7797d0678b5deef3c6c18f565d236bab
Tomasz Omeljaniuk <tomekomel@gmail.com>
2023-04-17 16:44:37 +0200

    feat: Function part 2

--- functions.ts
+++ functions.ts
@@ -1,3 +1,5 @@
+//Function Types
+
 // function type expression
 type Greet = (name: string) => void;
 
@@ -22,3 +24,42 @@ const s = firstElement(["a", "b", "c"]);
 const n = firstElement([1, 2, 3]);
 const u = firstElement([]);
 
+// we can constrain the acceptable types by adding extends
+function longest<Type extends { length: number }>(a: Type, b: Type): Type {
+    if (a.length > b.length) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+const longestArray = longest([1, 2, 3], [2, 3, 5, 6]);
+const wrongUsage = longestArray(2, 3);
+
+
+// Guidelines to write good function generics
+function firstElement1<Type>(arr: Type[]) {
+    return arr[0];
+}
+function firstElement2<Type extends any[]>(arr: Type) {
+    return arr[0];
+}
+
+// a: number (good)
+const a = firstElement1([1, 2, 3]);
+// b: any (bad)
+const b = firstElement2([1, 2, 3]);
+
+// 1. When possible, use the type parameter itself rather than constraining it
+// 2. Always use as few type parameters as possible
+// 3. If a type parameter only appears in one location, strongly reconsider if you actually need it
+
+// Function parameters:
+// - optional
+function f(n?: number) {
+    return n.toFixed();
+}
+// - default values
+function f10(n = 10): string {
+    return n.toFixed();
+}
