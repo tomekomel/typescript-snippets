1aa61bef9e0f15a9e371ab3773b11d49ba5b9b5d
Tomasz Omeljaniuk <tomekomel@gmail.com>
2023-04-21 16:27:12 +0200

    feat: Function overload

--- functions.ts
+++ functions.ts
@@ -1,14 +1,14 @@
-//Function Types
+// Function Types
 
-// function type expression
+// Function type expression
 type Greet = (name: string) => void;
 
-// if we want to describe something callable with properties
+// If we want to describe something callable with properties
 type GreetWithProperties = {
   properties: Record<string, string>;
   (name: string): void;
 };
-// usage
+// Usage:
 function greet(fn: GreetWithProperties) {
   console.log("Test Greet: ", fn.properties, fn("Patrick"));
 }
@@ -19,12 +19,12 @@ function firstElement<Type>(array: Type[]): Type | undefined {
 }
 // By adding Type we've created a link between the input and the output types
 
-// the type is inferred - chosen automatically by typescript
+// The type is inferred - chosen automatically by typescript
 const s = firstElement(["a", "b", "c"]);
 const n = firstElement([1, 2, 3]);
 const u = firstElement([]);
 
-// we can constrain the acceptable types by adding extends
+// We can constrain the acceptable types by adding extends
 function longest<Type extends { length: number }>(a: Type, b: Type): Type {
   if (a.length > b.length) {
     return a;
@@ -63,13 +63,47 @@ function f10(n = 10): string {
   return n.toFixed();
 }
 
-// optional parameters in callbacks, function statement:
+// Optional parameters in callbacks, function statement:
 function mapForEach(array: any[], callback: (arg: any, index?: number) => any) {
   for (let i = 0; i < array.length; i++) {
     callback(array[i]);
   }
 }
-// usage:
+// Usage:
 mapForEach([1, 2, 3], (a, i) => {
   console.log(i.toFixed());
 });
+
+// Function overloads, line 1 and 2 are overload signatures
+// 3 line is a function implementation signature compatible with overloads
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d!==undefined && y!==undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+// Usage:
+const d1 = makeDate(1123123);
+const d2 = makeDate(4, 5, 5);
+const d3 = makeDate(1, 3); // Error
+
+// Writing good overloads
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
+}
+
+// Error:
+len(Math.random() > 0.5 ? "hello" : [0]);
+
+// If both overloads have the same argument count and same return time it's better
+// to write non-overload version of this function:
+function len2(x: any[] | string) {
+  return x.length;
+}
+// Always prefer parameters with union types instead of overloads when possible ^^
+
